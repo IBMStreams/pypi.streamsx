d980f090512f2d77e70de3a53a51c77378d229c7
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-10-12 14:53:14 -0700

    Bump version, add requires and classifiers

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
   name = 'streamsx',
   packages = ['streamsx'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'IBM Streams Python Support',
   author = 'IBM Streams @ github.com',
   author_email = 'debrunne@us.ibm.com',
   url = 'https://github.com/IBMStreams/pypi.streamsx',
-  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.2',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.2.1',
   keywords = ['streams', 'ibmstreams'],
-  classifiers = [],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python'
+    
+  ],
+  install_requires=['requests'],
 )
