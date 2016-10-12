04874eba959dcea95f0d10e38c70943d09a903cf
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-10-12 15:22:51 -0700

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'streamsx',
   packages = ['streamsx'],
@@ -8,12 +8,14 @@ setup(
   author_email = 'debrunne@us.ibm.com',
   url = 'https://github.com/IBMStreams/pypi.streamsx',
   download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.2.1',
-  keywords = ['streams', 'ibmstreams'],
+  keywords = ['streams', 'ibmstreams', 'streaming'],
   classifiers = [
     'Development Status :: 3 - Alpha',
     'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python'
-    
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
   ],
   install_requires=['requests'],
 )
