f6f4ba8702a99c9ec4dbe6f0204652b3831cbf3a
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-10-12 14:17:00 -0700

    Bump version to have matching tag

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'streamsx',
   packages = ['streamsx'],
-  version = '0.1',
+  version = '0.2',
   description = 'IBM Streams Python Support',
   author = 'IBM Streams @ github.com',
   author_email = 'debrunne@us.ibm.com',
   url = 'https://github.com/IBMStreams/pypi.streamsx',
-  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.1',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.2',
   keywords = ['streams', 'ibmstreams'],
   classifiers = [],
 )
