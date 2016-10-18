a97910dfe02b7b1168282ea36f65481c4b1c32ee
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-10-17 22:33:26 -0700

    Switch to 1.5.1 ea1 release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'streamsx',
   packages = ['streamsx', 'streamsx.spl', 'streamsx.topology'],
   include_package_data=True,
-  version = '0.3.8',
+  version = '0.4.0',
   description = 'IBM Streams Python Support',
   author = 'IBM Streams @ github.com',
   author_email = 'debrunne@us.ibm.com',
   url = 'https://github.com/IBMStreams/pypi.streamsx',
-  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.3.8',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/0.4.0',
   keywords = ['streams', 'ibmstreams', 'streaming'],
   classifiers = [
     'Development Status :: 3 - Alpha',
@@ -18,5 +18,5 @@ setup(
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
   ],
-  install_requires=['requests'],
+  install_requires=['requests', 'future', 'dill'],
 )
