f8d05d1acec432bfcf687dce9109f4b31ef74f6f
Dan Debrunner <debrunne@us.ibm.com>
2018-02-21 09:49:40 -0800

    Add streamsx._streams package

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 setup(
   name = 'streamsx',
-  packages = ['streamsx', 'streamsx.spl', 'streamsx.topology', 'streamsx.scripts'],
+  packages = ['streamsx', 'streamsx.spl', 'streamsx.topology', 'streamsx.scripts', 'streamsx._streams'],
   include_package_data=True,
-  version = '1.9.0a1',
+  version = '1.9.0a2',
   description = 'IBM Streams Python Support',
   long_description = open('DESC.txt').read(),
   author = 'IBM Streams @ github.com',
   author_email = 'debrunne@us.ibm.com',
   license='Apache License - Version 2.0',
   url = 'https://github.com/IBMStreams/pypi.streamsx',
-  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/1.9.0a1',
+  download_url = 'https://github.com/IBMStreams/pypi.streamsx/tarball/1.9.0a2',
   keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics'],
   classifiers = [
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 3 - Alpha',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
